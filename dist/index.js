dfd35fca3a93ecc9aeeb5f8863be5d2daa3fe21b
mgreer <michael@tapptv.com>
2017-03-13 12:14:32 -0700

    actually export new actions

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,7 @@ exports.keenChartReducer = require('./AnalyticsReducer').default;
 exports.setKeenConfig = require('./AnalyticsActions').setKeenConfig;
 exports.stashResult = require('./AnalyticsActions').stashResult;
 exports.runQueries = require('./AnalyticsActions').runQueries;
+exports.setKeenStart = require('./AnalyticsActions').setKeenStart;
+exports.setKeenEnd = require('./AnalyticsActions').setKeenEnd;
+exports.setInterval = require('./AnalyticsActions').setInterval;
 exports.renderResults = require('./AnalyticsActions').renderResults;
