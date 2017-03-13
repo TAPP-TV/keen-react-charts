e90277d0d7498eec9692e835a006acacb078c3a7
mgreer <michael@tapptv.com>
2017-03-13 12:08:47 -0700

    add date and interval events

--- dist/AnalyticsActions.js
+++ dist/AnalyticsActions.js
@@ -1,10 +1,13 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.STORE_ORIGINAL_RESULTS = exports.STASH_RESULT = exports.RUNNING_QUERY = exports.RECEIVE_RESULTS = exports.SET_INTERVAL = exports.SET_DAYS_AGO = exports.SET_END = exports.SET_START = exports.SET_KEEN_CONFIG = undefined;
 exports.setKeenConfig = setKeenConfig;
+exports.setKeenStart = setKeenStart;
+exports.setKeenEnd = setKeenEnd;
+exports.setInterval = setInterval;
 exports.stashResult = stashResult;
 exports.runQueries = runQueries;
 exports.renderResults = renderResults;
@@ -26,103 +29,124 @@ var STASH_RESULT = exports.STASH_RESULT = 'STASH_RESULT';
 var STORE_ORIGINAL_RESULTS = exports.STORE_ORIGINAL_RESULTS = 'STORE_ORIGINAL_RESULTS';
 
 function setKeenConfig(projectId, readKey) {
-    return {
-        type: SET_KEEN_CONFIG,
-        projectId: projectId,
-        readKey: readKey,
-        receivedAt: (0, _moment2.default)().format()
-    };
+  return {
+    type: SET_KEEN_CONFIG,
+    projectId: projectId,
+    readKey: readKey,
+    receivedAt: (0, _moment2.default)().format()
+  };
 }
 
+function setKeenStart(dateTime) {
+  return {
+    type: SET_START,
+    start: (0, _moment2.default)(dateTime),
+    receivedAt: (0, _moment2.default)().format()
+  };
+}
+function setKeenEnd(projectId, readKey) {
+  return {
+    type: SET_END,
+    end: (0, _moment2.default)(dateTime),
+    receivedAt: (0, _moment2.default)().format()
+  };
+}
+function setInterval(interval) {
+  return {
+    type: SET_INTERVAL,
+    interval: interval,
+    receivedAt: (0, _moment2.default)().format()
+  };
+}
 //used to stash a result for use by another query
 function stashResult(key, timeframe, results) {
-    return {
-        type: STASH_RESULT,
-        key: key,
-        timeframe: timeframe,
-        results: results,
-        receivedAt: (0, _moment2.default)().format()
-    };
+  return {
+    type: STASH_RESULT,
+    key: key,
+    timeframe: timeframe,
+    results: results,
+    receivedAt: (0, _moment2.default)().format()
+  };
 }
 
 function runQueries(client, title, queryType, queries, resultsModifier) {
-    return function (dispatch) {
-        dispatch(runningQuery(title));
-        var promises = [];
-        var _iteratorNormalCompletion = true;
-        var _didIteratorError = false;
-        var _iteratorError = undefined;
+  return function (dispatch) {
+    dispatch(runningQuery(title));
+    var promises = [];
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
-        try {
-            for (var _iterator = queries[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                var query = _step.value;
+    try {
+      for (var _iterator = queries[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var query = _step.value;
 
-                promises.push(client.query(queryType, query));
-            }
-        } catch (err) {
-            _didIteratorError = true;
-            _iteratorError = err;
-        } finally {
-            try {
-                if (!_iteratorNormalCompletion && _iterator.return) {
-                    _iterator.return();
-                }
-            } finally {
-                if (_didIteratorError) {
-                    throw _iteratorError;
-                }
-            }
+        promises.push(client.query(queryType, query));
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
         }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
 
-        Promise.all(promises).then(function (res) {
-            dispatch(storeOriginalResults(title, res));
-            return dispatch(renderResults(title, res, resultsModifier));
-        }).catch(function (err) {
-            console.error('Error in querying results:', err);
-            return err;
-        });
-    };
+    Promise.all(promises).then(function (res) {
+      dispatch(storeOriginalResults(title, res));
+      return dispatch(renderResults(title, res, resultsModifier));
+    }).catch(function (err) {
+      console.error('Error in querying results:', err);
+      return err;
+    });
+  };
 }
 
 function renderResults(title, res, resultsModifier) {
-    return function (dispatch) {
-        var results = res;
-        if (resultsModifier) {
-            return resultsModifier(res).then(function (modResults) {
-                return dispatch(receiveQueryResults(title, modResults));
-            }).catch(function (err) {
-                console.error('Error in modifying results:', err);
-                return err;
-            });
-        }
-        if (res.length === 1) {
-            results = res[0];
-        }
-        return dispatch(receiveQueryResults(title, results));
-    };
+  return function (dispatch) {
+    var results = res;
+    if (resultsModifier) {
+      return resultsModifier(res).then(function (modResults) {
+        return dispatch(receiveQueryResults(title, modResults));
+      }).catch(function (err) {
+        console.error('Error in modifying results:', err);
+        return err;
+      });
+    }
+    if (res.length === 1) {
+      results = res[0];
+    }
+    return dispatch(receiveQueryResults(title, results));
+  };
 }
 
 //fSA
 function runningQuery(title) {
-    return {
-        type: RUNNING_QUERY,
-        title: title,
-        receivedAt: (0, _moment2.default)().format()
-    };
+  return {
+    type: RUNNING_QUERY,
+    title: title,
+    receivedAt: (0, _moment2.default)().format()
+  };
 }
 function storeOriginalResults(title, results) {
-    return {
-        type: STORE_ORIGINAL_RESULTS,
-        title: title,
-        results: results,
-        receivedAt: (0, _moment2.default)().format()
-    };
+  return {
+    type: STORE_ORIGINAL_RESULTS,
+    title: title,
+    results: results,
+    receivedAt: (0, _moment2.default)().format()
+  };
 }
 function receiveQueryResults(title, results) {
-    return {
-        type: RECEIVE_RESULTS,
-        title: title,
-        results: results,
-        receivedAt: (0, _moment2.default)().format()
-    };
+  return {
+    type: RECEIVE_RESULTS,
+    title: title,
+    results: results,
+    receivedAt: (0, _moment2.default)().format()
+  };
 }
--- src/AnalyticsActions.js
+++ src/AnalyticsActions.js
@@ -11,88 +11,106 @@ export const STASH_RESULT = 'STASH_RESULT';
 export const STORE_ORIGINAL_RESULTS = 'STORE_ORIGINAL_RESULTS';
 
 export function setKeenConfig(projectId, readKey) {
-    return {
-        type: SET_KEEN_CONFIG,
-        projectId: projectId,
-        readKey: readKey,
-        receivedAt: moment().format()
-    }
+  return {
+    type: SET_KEEN_CONFIG,
+    projectId: projectId,
+    readKey: readKey,
+    receivedAt: moment().format()
+  };
 }
 
+export function setKeenStart(dateTime) {
+  return {
+    type: SET_START,
+    start: moment(dateTime),
+    receivedAt: moment().format()
+  };
+}
+export function setKeenEnd(projectId, readKey) {
+  return {
+    type: SET_END,
+    end: moment(dateTime),
+    receivedAt: moment().format()
+  };
+}
+export function setInterval(interval) {
+  return {
+    type: SET_INTERVAL,
+    interval: interval,
+    receivedAt: moment().format()
+  };
+}
 //used to stash a result for use by another query
 export function stashResult(key, timeframe, results) {
-    return {
-        type: STASH_RESULT,
-        key: key,
-        timeframe: timeframe,
-        results: results,
-        receivedAt: moment().format()
-    }
+  return {
+    type: STASH_RESULT,
+    key: key,
+    timeframe: timeframe,
+    results: results,
+    receivedAt: moment().format()
+  };
 }
 
 export function runQueries(client, title, queryType, queries, resultsModifier) {
-    return dispatch => {
-        dispatch(runningQuery(title));
-        const promises = [];
-        for (const query of queries) {
-            promises.push(client.query(queryType, query))
-        }
-        Promise.all(promises)
-            .then((res) => {
-                dispatch(storeOriginalResults(title, res));
-                return dispatch(renderResults(title, res, resultsModifier));
-            })
-            .catch((err) => {
-                console.error('Error in querying results:', err);
-                return err;
-            })
+  return dispatch => {
+    dispatch(runningQuery(title));
+    const promises = [];
+    for (const query of queries) {
+      promises.push(client.query(queryType, query));
     }
+    Promise.all(promises)
+      .then(res => {
+        dispatch(storeOriginalResults(title, res));
+        return dispatch(renderResults(title, res, resultsModifier));
+      })
+      .catch(err => {
+        console.error('Error in querying results:', err);
+        return err;
+      });
+  };
 }
-
 
 export function renderResults(title, res, resultsModifier) {
-    return dispatch => {
-        let results = res;
-        if (resultsModifier) {
-            return resultsModifier(res)
-                .then((modResults) => {
-                    return dispatch(receiveQueryResults(title, modResults))
-                })
-                .catch((err) => {
-                    console.error('Error in modifying results:', err);
-                    return err;
-                })
-        }
-        if (res.length === 1) {
-            results = res[0];
-        }
-        return dispatch(receiveQueryResults(title, results));
+  return dispatch => {
+    let results = res;
+    if (resultsModifier) {
+      return resultsModifier(res)
+        .then(modResults => {
+          return dispatch(receiveQueryResults(title, modResults));
+        })
+        .catch(err => {
+          console.error('Error in modifying results:', err);
+          return err;
+        });
     }
+    if (res.length === 1) {
+      results = res[0];
+    }
+    return dispatch(receiveQueryResults(title, results));
+  };
 }
-
-
 
 //fSA
 function runningQuery(title) {
-    return {
-        type: RUNNING_QUERY,
-        title: title,
-        receivedAt: moment().format()
-    }
+  return {
+    type: RUNNING_QUERY,
+    title: title,
+    receivedAt: moment().format()
+  };
 }
 function storeOriginalResults(title, results) {
-    return {
-        type: STORE_ORIGINAL_RESULTS,
-        title: title,
-        results: results,
-        receivedAt: moment().format()
-    }
+  return {
+    type: STORE_ORIGINAL_RESULTS,
+    title: title,
+    results: results,
+    receivedAt: moment().format()
+  };
 }
 function receiveQueryResults(title, results) {
-    return {
-        type: RECEIVE_RESULTS,
-        title: title,
-        results: results,
-        receivedAt: moment().format()
-    }
+  return {
+    type: RECEIVE_RESULTS,
+    title: title,
+    results: results,
+    receivedAt: moment().format()
+  };
 }
